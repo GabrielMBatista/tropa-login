79c8b168f095c72f226889ca59a90cf1f8c9b83a
Gabriel Marques <gabbriel_gbl2@hotmail.com>
2025-06-06 00:05:16 -0300

    ajust login

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,10 +7,6 @@ import { Sessao } from "@/types/db";
 const dbPath = path.join(process.cwd(), "db.json");
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
-  }
-
   const { email, senha } = req.body;
 
   const adminEmail = process.env.NEXT_PUBLIC_AUTH_EMAIL;
